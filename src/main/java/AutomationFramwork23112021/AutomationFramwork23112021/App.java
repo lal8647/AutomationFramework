a6b8053bac9698907599c67b112bba1d58a8094f
lal8647 <lalmohan.kumar@outlook.com>
2021-11-23 12:53:11 +0530

    'automation'

---
+++ src/main/java/AutomationFramwork23112021/AutomationFramwork23112021/App.java
@@ -0,0 +1,13 @@
+package AutomationFramwork23112021.AutomationFramwork23112021;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
